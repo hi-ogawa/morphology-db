57670a343951dc9325c97265ee45242004454422
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2021-07-24 15:23:01 +0900

    feat: return single form for each unique word in Service.search

--- src/service.ts
+++ src/service.ts
@@ -7,15 +7,19 @@ type FuzzySearchResult = {
 
 export class Service {
   static async search(word: string): Promise<Lemma[]> {
+    // NOTE: there can be multiple lemmas matching given word. (e.g. "легко" matches "легко" and "легкий"),
+    //       probably due to annotation error.
     const rows = await Form.xfindqb({ word })
       .select("DISTINCT Form.lemmaId", "id")
       .getRawMany();
+    const ids = rows.map((row) => row.id);
     const result = await Lemma.xfindqb(
       {},
       { relations: ["forms", "forms.sentence"] }
     )
-      .whereInIds(rows.map((row) => row.id))
+      .whereInIds(ids)
       .orWhere(`Lemma.word = :word`, { word })
+      .groupBy("Lemma__forms.word") // Limit to single form for each unique word
       .getMany();
     return result;
   }
